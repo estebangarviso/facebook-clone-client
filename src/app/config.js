81c40447fa9948d28cc979d702296c12561b71c6
Esteban Garviso <e.garvisovenegas@gmail.com>
2022-08-03 10:47:14 -0400

    update env

--- src/app/config.js
+++ src/app/config.js
@@ -1,6 +1,6 @@
 // export const BACKEND_URL = 'http://localhost:3002';
 // export const TOKEN_EXPIRATION_WARN_TIME_MILISECONDS = 180000; // Watch out for this value, it's used in the frontend to warn the user about the token expiration need to be lower than server the token expiration time
 // export const WS_URL = 'ws://localhost:3002';
-export const BACKEND_URL = process.env.BACKEND_URL;
-export const TOKEN_EXPIRATION_WARN_TIME_MILISECONDS = process.env.TOKEN_EXPIRATION_WARN_TIME_MILISECONDS;
-export const WS_URL = process.env.WS_URL;
+export const BACKEND_URL = process.env.BACKEND_URL || 'http://localhost:3002';
+export const TOKEN_EXPIRATION_WARN_TIME_MILISECONDS = process.env.TOKEN_EXPIRATION_WARN_TIME_MILISECONDS || 180000;
+export const WS_URL = process.env.WS_URL || 'ws://localhost:3002';
